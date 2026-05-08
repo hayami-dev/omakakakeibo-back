25efd40275e42645fd92745394f9fc09140ba31c
hayami <225549499+hayami-dev@users.noreply.github.com>
2026-05-08 23:11:08 +0900

    1st commit

---
+++ src/main/java/com/example/app/OmakakakeiboBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmakakakeiboBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmakakakeiboBackApplication.class, args);
+	}
+
+}
