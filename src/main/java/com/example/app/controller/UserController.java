d2e49d1646b480ccb5c00fdc7c305d7bec061304
sss <sss@sss.com>
2026-06-25 08:58:46 +0900

    新規登録フロー：認証トークンの発行機能を実装

--- src/main/java/com/example/app/controller/UserController.java
+++ src/main/java/com/example/app/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.example.app.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import jakarta.validation.Valid;
 
 import org.mindrot.jbcrypt.BCrypt;
@@ -11,7 +14,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.app.domain.DtoRegisterRequest;
+import com.example.app.domain.DtoUserAuthToken;
 import com.example.app.domain.User;
+import com.example.app.domain.UserAuthToken;
 import com.example.app.mapper.UserMapper;
 
 import lombok.RequiredArgsConstructor;
@@ -23,6 +28,54 @@ import lombok.RequiredArgsConstructor;
 public class UserController {
 
 	private final UserMapper userMapper;
+
+	// ユーザーの新規登録用のトークン発行
+	@PostMapping("/register-request")
+	public ResponseEntity<String> requestRegister(
+			@Valid @RequestBody DtoRegisterRequest request) {
+		// メアドを取得
+		String email = request.getEmail();
+
+		// トークン発行
+		String token = java.util.UUID.randomUUID().toString();
+
+		// DBにトークンを保存
+		userMapper.insertToken(email, token);
+
+		// 開発用：コンソールに認証用のフロントURLを出力
+
+		System.out.println("==================================================");
+		System.out.println("【開発用】認証メールを送信しました。以下のURLにアクセスしてください：");
+		System.out.println("http://localhost:5173/auth/register/verify?token=" + token);
+		System.out.println("==================================================");
+
+		return ResponseEntity.ok().body("Success");
+
+	}
+
+	// トークンを検証
+	@PostMapping("/verify-token")
+	public ResponseEntity<?> verifyToken(
+			@Valid @RequestBody DtoUserAuthToken request) {
+
+		// フロントから届いたトークンを取り出す
+		String token = request.getToken();
+
+		// Mapperを使って検証
+		UserAuthToken validTokenData = userMapper.findValidToken(token);
+
+		// 見つからなかった場合
+		if (validTokenData == null) {
+			return ResponseEntity.badRequest().body("無効なトークン、または有効期限切れです。");
+		}
+
+		// 見つかった場合Mapに詰める
+		Map<String, String> response = new HashMap<>();
+		response.put("email", validTokenData.getNewEmail());
+
+		return ResponseEntity.ok(response);
+
+	}
 
 	// ユーザー新規登録
 	@PostMapping("/register")
--- src/main/java/com/example/app/mapper/UserMapper.java
+++ src/main/java/com/example/app/mapper/UserMapper.java
@@ -9,6 +9,7 @@ import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import com.example.app.domain.User;
+import com.example.app.domain.UserAuthToken;
 
 @Mapper
 public interface UserMapper {
@@ -35,4 +36,20 @@ public interface UserMapper {
 	@Options(useGeneratedKeys = true, keyProperty = "userId")
 	void insertUser(
 			User user);
+
+	// 新規登録用トークンを発行、保存する
+	// token_typeは 'email_verify'、new_emailカラムに
+	// 入力されたメアドを一時保存
+	@Insert("INSERT INTO user_auth_tokens "
+			+ "(token_type, token_hash, new_email, expires_at) "
+			+ "VALUES('email_verify', #{token}, #{email}, "
+			+ "DATE_ADD(NOW(), INTERVAL 30 MINUTE))")
+	void insertToken(@Param("email") String email, @Param("token") String token);
+
+	// トークンをDBから探す
+	@Select("SELECT * FROM user_auth_tokens "
+			+ "WHERE token_hash = #{token} "
+			+ "AND token_type = 'email_verify' "
+			+ "AND expires_at > NOW()")
+	UserAuthToken findValidToken(@Param("token") String token);
 }
